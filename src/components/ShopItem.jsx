e40f316dcdf677d6e69ea8236bd65139ddae71ac
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-09 21:44:36 +1100

    Add validation to shop item component

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -9,31 +9,48 @@ const ShopItem = ({ id, name, image, initialQuantity = 1, cartView }) => {
     updateQuantityOfProductFromCart,
   } = useOutletContext();
 
+  const handleAddToCart = (event, id, name, quantity, image) => {
+    event.preventDefault();
+    addProductToCart(id, name, quantity, image);
+  };
+
+  const handleQuantityUpdate = (event) => {
+    const input = event.target;
+    const newQuantity = input.value;
+
+    if (input.validity.valid) {
+      setQuantity(newQuantity);
+      if (cartView) {
+        updateQuantityOfProductFromCart(id, newQuantity);
+      }
+    } else {
+      input.reportValidity();
+    }
+  };
+
   return (
     <li>
       <img src={image} alt={`album artwork for ${name}`} />
       <p>{name}</p>
-      <label htmlFor="quantity">Quantity</label>
-      <input
-        type="number"
-        id={`${id}-quantity`}
-        defaultValue={initialQuantity}
-        onChange={(event) => {
-          setQuantity(event.target.value);
-          if (cartView) {
-            updateQuantityOfProductFromCart(id, event.target.value);
-          }
-        }}
-      ></input>
-      {cartView ? (
-        <button onClick={() => deleteProductFromCart(id)}>
-          Remove from cart
-        </button>
-      ) : (
-        <button onClick={() => addProductToCart(id, name, quantity, image)}>
-          Add to cart
-        </button>
-      )}
+      <form
+        onSubmit={(event) => handleAddToCart(event, id, name, quantity, image)}
+      >
+        <label htmlFor={`${id}-quantity`}>Quantity</label>
+        <input
+          type="number"
+          min={1}
+          id={`${id}-quantity`}
+          defaultValue={initialQuantity}
+          onChange={(event) => handleQuantityUpdate(event)}
+        ></input>
+        {cartView ? (
+          <button type="button" onClick={() => deleteProductFromCart(id)}>
+            Remove from cart
+          </button>
+        ) : (
+          <button type="submit">Add to cart</button>
+        )}
+      </form>
     </li>
   );
 };
