d9a7296f70ab014af77d7e9c94570d852ee62b5e
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-14 14:09:20 +1100

    Update styling for error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,15 @@
+import styles from "../styles/ErrorPage.module.css";
+import { Link } from "react-router";
+import logo from "/waxdisc-logo.png";
+
 const ErrorPage = () => {
   return (
-    <>
+    <div className={styles.container}>
       <p>I think you might be lost 🧐</p>
-    </>
+      <Link className={styles.homeLink} to="/">
+        <img src={logo} alt="WAXDISC logo"></img>Return Home
+      </Link>
+    </div>
   );
 };
 
